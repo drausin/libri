53051413532398f909da8a512a8a56715277a3b2
Drausin Wulsin <daedalus2718@gmail.com>
2017-03-01 08:48:27 -0500

    add TestConnector tests

---
+++ libri/librarian/server/peer/testing_test.go
@@ -0,0 +1,32 @@
+package peer
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTestConnector_Connect(t *testing.T) {
+	c := &TestConnector{}
+	client, err := c.Connect()
+	assert.Nil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestTestConnector_Disconnect(t *testing.T) {
+	c := &TestConnector{}
+	err := c.Disconnect()
+	assert.Nil(t, err)
+}
+
+func TestTestErrConnector_Connect(t *testing.T) {
+	c := &TestErrConnector{}
+	client, err := c.Connect()
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestTestErrConnector_Disconnect(t *testing.T) {
+	c := &TestErrConnector{}
+	err := c.Disconnect()
+	assert.Nil(t, err)
+}
