9f8fd50e65836449b25bc2b8b3fb675c8beb2b94
Drausin Wulsin <daedalus2718@gmail.com>
2017-08-21 11:51:02 -0400

    set version from version string

--- version/version.go
+++ version/version.go
@@ -4,13 +4,15 @@ import (
 	"github.com/blang/semver"
 )
 
+// Version is the current version of this repo.
+var Version semver.Version
+
+var versionString = "0.1.0"
+var isSnapshot = true
 var snapshot = semver.PRVersion{VersionStr: "snapshot"}
 
-// Version is the current version of this repo.
-var Version = semver.Version{Major: 0, Minor: 1, Patch: 0}
-var isSnapshot = true
-
 func init() {
+	Version = semver.MustParse(versionString)
 	if isSnapshot {
 		Version.Pre = []semver.PRVersion{snapshot}
 	}
