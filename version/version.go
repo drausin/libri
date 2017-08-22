85f739400d28ddf631f16e88873bf470a6bbc02b
Drausin Wulsin <daedalus2718@gmail.com>
2017-08-22 08:05:37 -0400

    release prep (#116)

--- version/version.go
+++ version/version.go
@@ -5,8 +5,15 @@ import (
 )
 
 // Version is the current version of this repo.
-var Version = semver.Version{
-	Major: 0,
-	Minor: 1,
-	Patch: 0,
+var Version semver.Version
+
+var versionString = "0.1.0"
+var isSnapshot = true
+var snapshot = semver.PRVersion{VersionStr: "snapshot"}
+
+func init() {
+	Version = semver.MustParse(versionString)
+	if isSnapshot {
+		Version.Pre = []semver.PRVersion{snapshot}
+	}
 }
