73f05e0ba23ac058f4fa6e815736e1bb04ada666
Drausin Wulsin <daedalus2718@gmail.com>
2017-10-15 11:55:07 -0400

    move RocksDB build to separate script

---
+++ build/install-rocksdb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Download and install RocksDB on the *nix system.
+
+ROCKSDB_VERSION='5.8'
+
+# download
+wget https://github.com/facebook/rocksdb/archive/v${ROCKSDB_VERSION}.zip -O /tmp/rocksdb-${ROCKSDB_VERSION}.zip
+unzip /tmp/rocksdb-${ROCKSDB_VERSION}.zip -d /tmp
+cd /tmp/rocksdb-${ROCKSDB_VERSION}
+
+# creates librocksdb.a
+make static_lib
+
+# installs librocksdb.a at /usr/local/lib/librocksdb.a and source at /usr/local/include/rocksdb/
+make install
+
+# temporary, local container: cf2a62e39be56d937ecb6c6c6fd8dbb8
+md5sum /usr/local/lib/librocksdb.a
+
+# clean up, keeping
+rm /tmp/rocksdb-${ROCKSDB_VERSION}.zip
+rm -r /tmp/rocksdb-${ROCKSDB_VERSION}
