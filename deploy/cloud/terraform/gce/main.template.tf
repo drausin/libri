bc633f4ddd2c299d0d72da14dc85abc007491db3
Drausin Wulsin <daedalus2718@gmail.com>
2017-11-12 01:00:24 -0500

    add limits and monitoring ports to template

--- deploy/cloud/terraform/gce/main.template.tf
+++ deploy/cloud/terraform/gce/main.template.tf
@@ -19,9 +19,17 @@ variable "librarian_disk_size_gb" {
   description = "size (GB) of persistant disk used by each librarian"
 }
 
-# TODO (drausin) add after parameterizing in k8s template
-# - pod ram & cpu limits
-# - libri version
+variable "librarian_libri_version" {
+  description = "libri version (e.g., 0.1.0, latest, snapshot) to use for librarian container"
+}
+
+variable "librarian_cpu_limit" {
+  description = "librarian container CPU limit (e.g., 500m, 0.5, 1)"
+}
+
+variable "librarian_ram_limit" {
+  description = "librarian container RAM limit (e.g., 500M, 1G)"
+}
 
 variable "num_cluster_nodes" {
   description = "current number of cluster nodes"
@@ -33,17 +41,22 @@ variable "cluster_node_machine_type" {
 
 variable "librarian_public_port_start" {
   description = "public port for librarian-0 service"
-  default = 30100
 }
 
 variable "librarian_local_port" {
   description = "local port for each librarian instance"
-  default = 20100
 }
 
 variable "librarian_local_metrics_port" {
   description = "local metrics port for each librarian instance"
-  default = 20200
+}
+
+variable "grafana_port" {
+  description = "port for Grafana service"
+}
+
+variable "prometheus_port" {
+  description = "port for Prometheus service"
 }
 
 module "{{ .ClusterName }}" {
