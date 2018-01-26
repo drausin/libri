6040aabe9a095a192b85a24e994dec08a1fcd06a
Drausin Wulsin <daedalus2718@gmail.com>
2018-01-26 12:17:56 -0500

    add initial version of release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,6 @@
 
 set -eou pipefail
 
-release_version=$(grep "semverString = " version/version.go | sed -r "s|.+semverString = \"(.+)\"$|\1|g")
 
 usage() {
     echo 'Libri release script'
@@ -12,20 +11,24 @@ usage() {
 }
 
 start() {
-    echo -n "Libri release script\n"
-    echo "enter version to release [${release_version}]:"
+    echo -e 'Starting libri release\n'
+    release_version=$(grep "semverString = " version/version.go | sed -r "s|.+semverString = \"(.+)\"$|\1|g")
+    echo "enter version to start releasing [${release_version}]:"
     read release_version
     echo "enter next version after this release (e.g., 0.4.0):"
     read next_version
 
     release_branch="release/${release_version}"
-    echo "cutting release branch ${release_branch} from develop..."
+    set +x
+    git checkout develop
+    git pull
     git checkout -b ${release_branch} develop
+    set -x
 
     echo 'please draft release notes in CHANGELOG.md [press ENTER when finished]'
     read
 
-    echo 'please copy the CHANGELOG.md release notes into a new Github release (https://github.com/drausin/libri/releases/new) [press ENTER when finished]'
+    echo 'please copy the CHANGELOG.md release notes into a new Github release draft (https://github.com/drausin/libri/releases/new) [press ENTER when finished]'
     read
 
     set +x
@@ -35,11 +38,35 @@ start() {
     git commit -am "bump to next (in progress) version"
     git push origin develop
     set -x
-    echo ''
+    echo -e 'Started libri release. Run\n'
+    echo '  ./release.sh finish\n'
+    echo 'to finish when ready.'
+}
+
+finish() {
+    echo -e 'Finishing libri release\n'
+    release_version=$(git branch --list 'release/*' --sort=-committerdate | head -1 | sed -r "s|^release/(.+)$|\1|g")
+    echo "enter version to finish releasing [${release_version}]:"
+    read release_version
+
+    release_branch="release/${release_version}"
+    set +x
+    git checkout master
+    git merge --no-ff ${release_branch}
+    git tag -a "v${release_version}" -m "release v${release_version}"
+    git checkout develop
+    git pull
+    git merge --no-ff ${release_branch} -s recursive -X ours
+    git branch -d ${release_branch}
+    git push origin develop
+    git checkout master
+    git push origin master --tags
+    set -x
+    echo "Finished libri release. Please attach the Github draft release notes to tag v${release_version}"
+
 }
 
 cmd=${1}
-
 case ${cmd} in
     start)
         start ;;
