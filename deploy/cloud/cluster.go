1e98523667d5b4bf1c43990b37e5387ac01ed536
Drausin Wulsin <daedalus2718@gmail.com>
2018-08-24 20:23:48 -0400

    ignore gosec lint in certain places

--- deploy/cloud/cluster.go
+++ deploy/cloud/cluster.go
@@ -313,7 +313,7 @@ func writePropsFile(config TFConfig, clusterDir, templateDir string) {
 }
 
 func tfCommand(clusterDir string, subcommand string) {
-	tfInitCmd := exec.Command("terraform", subcommand) // nolint: gas
+	tfInitCmd := exec.Command("terraform", subcommand) // nolint: gosec
 	tfInitCmd.Stdin = os.Stdin
 	tfInitCmd.Stdout = os.Stdout
 	tfInitCmd.Stderr = os.Stderr
@@ -323,7 +323,7 @@ func tfCommand(clusterDir string, subcommand string) {
 }
 
 func kubeApply(clusterDir string, dryRun bool) {
-	tfInitCmd := exec.Command("kubectl", "apply", "-f", kubeConfigFilename) // nolint: gas
+	tfInitCmd := exec.Command("kubectl", "apply", "-f", kubeConfigFilename) // nolint: gosec
 	if dryRun {
 		tfInitCmd.Args = append(tfInitCmd.Args, "--dry-run")
 	}
@@ -385,7 +385,7 @@ func getTFFlags(clusterDir string) variables.FlagFile {
 }
 
 func writeKubeConfigMaps() {
-	maybeDelete := exec.Command("sh", "-c", "kubectl get configmap --no-headers | "+ // nolint: gas
+	maybeDelete := exec.Command("sh", "-c", "kubectl get configmap --no-headers | "+ // nolint: gosec
 		"grep 'config' | awk '{print $1}' | xargs -I {} kubectl delete configmap {}")
 	maybeDelete.Stdout = os.Stdout
 	err := maybeDelete.Run()
@@ -395,7 +395,7 @@ func writeKubeConfigMaps() {
 	for _, name := range resourceNames {
 		configName := fmt.Sprintf("%s-config", name)
 		fromFile := fmt.Sprintf("--from-file=kubernetes/config/%s", name)
-		create := exec.Command("kubectl", "create", "configmap", configName, fromFile) // nolint: gas
+		create := exec.Command("kubectl", "create", "configmap", configName, fromFile) // nolint: gosec
 		create.Stdout = os.Stdout
 		err = create.Run()
 		maybeExit(err)
